ded9682db7bc11666f3f4d40d9d667437b30613d
ieavci <iavcci123@gmail.com>
2024-08-03 17:41:11 +0300

    First Commit

---
+++ Proffessional-Portfolio/Proffessional-Portfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proffessional_Portfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
